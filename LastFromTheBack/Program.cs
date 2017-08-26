69fda8435a09010a1a6b6574d00a814a17ef6d5f
Simi <adegokesimi@gmail.com>
2017-08-26 13:07:22 +0100

    Final-day

---
+++ LastFromTheBack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastFromTheBack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //third from last element in an array
+            int[] myArray = { 1, 2, 3, 4, 5, 9,10,12, 15,37 ,98, 08};
+                       
+            Console.WriteLine(myArray[myArray.Length - 3]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> shoppingList = new LinkedList<string>();
+            shoppingList.AddFirst("Milk");
+
+            LinkedListNode<string> node = shoppingList.Find("Milk");
+
+            shoppingList.AddBefore(node, "Bread");
+            shoppingList.AddAfter(node, "Tead");
+            shoppingList.AddLast("Ginger");
+
+            foreach (var item in shoppingList)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+   
+}
