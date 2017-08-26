d99bd0859f117d56ecabe94dd8585319ec334dc1
Simi <adegokesimi@gmail.com>
2017-08-26 14:11:30 +0100

    final-day

---
+++ Armstrong/Armstrong/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armstrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1634;
+            string numText = number.ToString();
+            List<int> charToNum = new List<int>();
+            double sum = 0;
+
+            char[] characters = numText.ToCharArray();
+
+            foreach (var item in characters)
+            {
+                int convertToInt = (int)char.GetNumericValue(item);
+                charToNum.Add(convertToInt);
+                //Console.WriteLine(item);
+                //Console.WriteLine(convertToInt);
+            }
+
+            double count = charToNum.Count();
+            foreach (var item in charToNum)
+            {
+                sum = sum + Math.Pow(item, count);
+                //Console.WriteLine(item);
+            }
+            //Console.WriteLine(numText);
+            //Console.WriteLine(sum);
+
+            if (number== sum )
+            {
+                Console.WriteLine("Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("Not an Armstrong Number");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
