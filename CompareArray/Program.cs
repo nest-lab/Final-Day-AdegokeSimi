dd572c146920ad8ed3af244e213c7006bffec7a2
Simi <adegokesimi@gmail.com>
2017-08-26 13:22:46 +0100

    final-day

---
+++ CompareArray/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5,6 };
+            int[] array2 = { 2, 3, 1, 0, 5 };
+
+            List<int> newArray = new List<int>();
+            newArray = array1.ToList();
+
+            List<int> found = new List<int>();
+            List<int> Notfound = new List<int>();
+
+            foreach (var num1 in array1)
+            {
+                foreach (var num2 in array2)
+                {
+                    if (num1 == num2)
+                    {
+                        found.Add(num1);
+                    }
+                    else
+                    {
+                        Notfound.Add(num1);
+                    }
+               }
+            }
+
+        
+            foreach (var item in array1)
+            {
+                foreach (var item1 in found)
+                {
+                    if (item == item1)
+                    {
+                        newArray.Remove(item);
+                    }
+                }
+            }
+
+            //
+            Console.WriteLine("The following element cannot be found;");
+            foreach (var item in newArray)
+            {
+                
+                Console.WriteLine(item);
+            }
+           
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ SecondHighest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHighest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array2 = { 2, 3, 1, 0, 5,10,98,79 };
+
+            int[] newArray = array2.OrderByDescending(c => c).ToArray();
+            
+            Console.WriteLine(newArray[1]);
+            Console.ReadLine();
+        }
+    }
+}
